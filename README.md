429e0a8eef7166419e56d3ecb408b2c76c53c68d
jabesv <vjabes0@gmail.com>
2022-05-16 07:45:00 -0400

    README UPDATED

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # Snake Game
 
 ## Description
-This game consist in a snake in a square space eating apples, every time an apple shows up the snake needs to go to that apple. We need to use the arrows in the keyboard to lead the snake to the apple.
-if the snake hits the walls of the square area or if it hits itself, the game is over.
+This game consist in a snake eating apples that appear randomly in a square space. Every time an apple shows up the snake needs to go to that apple. You can use the  arrows in the keyboard to lead the snake to the apple.
+if the snake hits the walls of the square area or if it hits itself, the game is over. Your score will be the amount of apples you eat without hitting the walls or crashing the snake whit its body. 
 
 
 ### Wireframe
 link to my wireframe:
-https://wireframe.cc/BMq7IV
+https://wireframe.cc/J6e24f
 
 
-The design will be inproved 
+###  Technologies
+The game have a HTML file with a H1 for the title, the canvas tag, and a button to reset the game. Also a CSS file with a small code for styles of body, canvas, and H1. Lastly a JS file with the code to run the game. 
+
+The JS file contains declarations of variables to maintain the structure of the canvas, the snake and the apples. With the use of conditions to evaluate direction of the snake (up, down, left, right), for loops to and a class to increase the size of the tail and position of the snake. Event listeners to detect the arrows. This intructions contained in a several fuctions.
+
+
+### Unsolved Problems
+Need to increase speed every time the snake eats an apple.
+Wanted to create a high score record. So that will be the challenge for the player to beat.
+
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 console.log("conected!")
 
+//the canvas is conected to a variable and the contex to 'paint in it is declare 2d
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+//function to reset the game
+function reloadPage() {
+    location.reload();    
+}
 
+
+//class to add new parts to the tail
 class SnakePart {
     constructor(x, y) {
         this.x = x;
@@ -11,42 +18,46 @@ class SnakePart {
     }
 }
 
+//initial speed  
 let speed = 7; // times in a second
 
+//stablisment of the area in the canvas
 let tileCount = 20;
 let tileSize = canvas.width / tileCount - 2;
 
+//snake 
 let headX = 10;
 let headY = 10;
-const snakeParts = [];
-let tailLength = 2;
+const snakeParts = []; //this array will help us to move and increase the body of the snake. will be use will the class SnakePart
+let tailLength = 2; //The snake starts with a tail with 2 tiles
 
-
-let appleX = 5;
+// variable apple and initial position
+let appleX = 5;     
 let appleY = 5;
 
-
+//declaration of the variable x and y velocity
 let xVelocity = 0;
 let yVelocity = 0;
 
+//initial score
 let score = 0;
 
 // game loop
 function drawGame(){
     //console.log('game on') 
+
     changeSnakePosition();
     let result = isGameOver();
     if (result) {
         return;
     }    
    
-    
-
     clearScreen();
     
-    
     checkAppleCollusion();
+
     drawApple();
+
     drawSnake();
 
     drawScore();
@@ -54,23 +65,44 @@ function drawGame(){
     setTimeout(drawGame, 1000/ speed);
 }
 
+
+
 function isGameOver() {
     let gameOver = false;
+    // this conditional void the program to start in game over
+     if ( yVelocity === 0 && xVelocity === 0 ) {
+         return false;
+     }
 
-    //walls at the left
+    // the next conditions check if the snake hits any wall:
+    //wall at the left
     if( headX < 0 ) {
         gameOver = true;
-    } else if ( headX ===tileCount ) {
+     } 
+     //wall at the right
+     else if ( headX ===tileCount ) {
         gameOver = true
-    } else if (headY < 0 ) { 
-        gameOver = true
-    } else if ( headY === tileCount ) {
-        gameOver = true
-    }
-
+     }   
+     // top wall
+     else if (headY < 0 ) {    
+         gameOver = true
+     } 
+     // bottom wall
+     else if ( headY === tileCount ) {
+         gameOver = true
+     }
     
+     // this loop checks if the snake hits itself
+     for (let i =0; i < snakeParts.length; i++) {
+         let part = snakeParts[i];
+         if (part.x === headX && part.y === headY) {
+             gameOver = true;
+             break;
+         }
+     }
 
 
+    //'Game over' is displayed
      if (gameOver) {
         ctx.fillStyle = 'white';
         ctx.font = "50px Verdana";
@@ -80,48 +112,51 @@ function isGameOver() {
     return gameOver;
 }
 
- 
-
+//this function that places the score in the canvas
 function drawScore() {
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "white"; 
     ctx.font = "10px Verdana"
     ctx.fillText("Score  " + score, canvas.width-50, 10);
 }
+
+
 // gets the canvas ready with color and clean.
 function clearScreen() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height); // fills the canvas width and height
 }
 
+    //funtion to draw the snake and keep size of its tail
 function drawSnake() {
     
+    //tail of the snake
     ctx.fillStyle = 'green';
     for(let i = 0; i< snakeParts.length; i++) {
         let part = snakeParts [i];
         ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
     }
-
-    snakeParts.push(new SnakePart(headX, headY)); // put an item next to the head
+        //using the class the snake add new pieces to its tail every time eats an apple
+    snakeParts.push(new SnakePart(headX, headY)); // add an item next to the head
     if (snakeParts.length > tailLength){
         snakeParts.shift(); //remove the further item from the snake parts if we have more than tail size
     }
-
+        //this lines keep crontrol of the head of the snake
     ctx.fillStyle = 'orange';
     ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 
 }
 
-
+    //funtion to keep control of the snake new positions usin the coordenates
 function changeSnakePosition() {
     headX = headX + xVelocity;   //xVelocity can be positive or negative to move left or right
-    headY = headY + yVelocity;   //yVelocity can be positive or negative
+    headY = headY + yVelocity;   //yVelocity can be positive or negative to move the head up or down
 
     //console.log(headX)
     //console.log(headY)
 }
 
 
-    //draw the apple base 
+     //draw the apple base using the contex 'ctx' variable to paint the apple
 function drawApple(){
     ctx.fillStyle = 'red';
     ctx.fillRect ( appleX * tileCount, appleY * tileCount, tileSize, tileSize);    
@@ -140,7 +175,7 @@ function checkAppleCollusion() {
 }
 
 
-
+//Event listener to activate and move the snake
 document.body.addEventListener('keydown', keyDown);
 
 
@@ -153,7 +188,6 @@ function keyDown(event) {
         yVelocity = -1;
         xVelocity = 0;
     }
-
     //arrow down
     if (event.keyCode == 40){
         if (yVelocity == -1)
@@ -161,7 +195,6 @@ function keyDown(event) {
         yVelocity = 1;
         xVelocity = 0;
     }
-
     // arrow left 
     if (event.keyCode == 37) {
         if (xVelocity == 1)
@@ -169,18 +202,37 @@ function keyDown(event) {
         yVelocity = 0;
         xVelocity = -1;
     }
-
     //arrow right
     if (event.keyCode == 39) {
         if (xVelocity == -1)
             return
         yVelocity = 0;
         xVelocity = 1;
-    }
+    }   
 }      
 
-
-
+//call function to start game
 drawGame();
 
 
+
+
+//===============================================================
+// const button = document.querySelector('#reset')
+// console.log(button)
+// button.addEventListener("click", reset);
+
+// function reset(){
+//  drawGame()
+//  }
+
+//document.addEventListener('click', reload())
+// function reset() {
+// }
+
+//const reload = document.getElementById('reset')
+//reset.addEventListener('click', reset)
+// reset.onclick = drawSnake()
+
+//let button = document.querySelector("#reset")
+//button.addEventListener('click', reset());
